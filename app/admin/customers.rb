4dc5abc88a2be1d8aceba544d6140cf300fb28d5
Mitchell Cook <m.cook2434@gmail.com>
2020-11-04 14:29:06 -0600

    added image attachment for customer model

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -4,7 +4,16 @@ ActiveAdmin.register Customer do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :full_name, :phone_number, :email, :notes
+  permit_params :full_name, :phone_number, :email, :notes, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
   #
   # or
   #
